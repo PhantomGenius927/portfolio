fd026d94a4752f6b4ecdd1fa945db0f1c76a0382
PhantomGenius927 <73685955+PhantomGenius927@users.noreply.github.com>
2023-06-21 15:50:14 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+window.addEventListener('DOMContentLoaded', startFunction);
+
+const likeButton = document.getElementsByClassName("button1")
+/*let
+*var
+*/
+
+
+function startFunction {
+    likeButton.addEventListener('click', changeStyle)
+}
+
+function changeStyle() {
+    subscribeButton.style.background = "black";
+}
